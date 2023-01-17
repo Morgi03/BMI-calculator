150276f4144af949bf57cf492b2f58a2ba5ce83f
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-17 10:48:54 +0100

    App kinézetének elkészítáése, Bmi kiszámítása

--- bmi-calculator/src/App.tsx
+++ bmi-calculator/src/App.tsx
@@ -1,31 +1,105 @@
-import React from "react";
+import React, { ChangeEvent, Component, FormEventHandler } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div id="content">
-      <div className="header">
-        <p>
-          Írja be cm-ben számított testmagasságát (cipő nélkül mérve):
-          <span className="inp">
-            <input type="number" id="magassag" />
-            <span className="mertekegyseg">cm</span>
-          </span>
-        </p>
-        <p>
-          Írja be kg-ban számított testsúlyát:{" "}
-          <span className="inp">
-            <input type="number" id="suly" />
-            <span className="mertekegyseg">kg</span>
-          </span>
-        </p>
-      </div>
-      <div>
-        <h2 className="orangehp">A ÖN BMI ÉRTÉKE:</h2>
-      </div>
-    </div>
-  );
+interface State {
+  bmival: number;
+  height: number;
+  weight: number;
 }
 
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      bmival: 0,
+      height: 0,
+      weight: 0,
+    };
+  }
+
+  heightEvent = () => {
+    let newheight = parseInt((document.getElementById('magassag') as HTMLInputElement).value);
+    this.setState({
+      height: newheight,
+    });
+  };
+
+  weightEvent = () => {
+    let newweight = parseInt((document.getElementById('suly') as HTMLInputElement).value);
+    this.setState({
+      weight: newweight,
+    });
+  };
+
+  bmicalc = () => {
+    this.setState({
+      bmival:
+        Math.round(
+          (this.state.weight / Math.pow(this.state.height / 100, 2)) * 100
+        ) / 100,
+    });
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div id="content">
+        <div className="header">
+          <p>
+            Írja be cm-ben számított testmagasságát (cipő nélkül mérve):
+            <span className="inp">
+              <input
+                type="number"
+                id="magassag"
+                onInput={this.heightEvent}
+              />
+              <span className="mertekegyseg">cm</span>
+            </span>
+          </p>
+          <p>
+            Írja be kg-ban számított testsúlyát:
+            <span className="inp">
+              <input
+                type="number"
+                id="suly"
+                onInput={this.weightEvent}
+              />
+              <span className="mertekegyseg">kg</span>
+            </span>
+          </p>
+          <button onClick={this.bmicalc}>Számítás</button>
+        </div>
+        <div>
+          <h2 className="orangehp">A ÖN BMI ÉRTÉKE: {this.state.bmival}</h2>
+          <table>
+            <tr className={(this.state.bmival < 18.5) ? "selection" : "nothing"}>
+              <td className="firstcol">18,5 vagy kevesebb</td>
+              <td>SOVÁNY</td>
+            </tr>
+            <tr className={(this.state.bmival < 18.5) ? "selection" : "nothing"}>
+              <td className="firstcol">18,5 - 24,9</td>
+              <td>NORMÁL</td>
+            </tr>
+            <tr className={(this.state.bmival < 18.5) ? "selection" : "nothing"}>
+              <td className="firstcol">30 - 34,9</td>
+              <td>I. FOKÚ ELHÍZÁS</td>
+            </tr>
+            <tr className={(this.state.bmival < 18.5) ? "selection" : "nothing"}>
+              <td className="firstcol">35 - 39,9</td>
+              <td>II. FOKÚ ELHÍZÁS</td>
+            </tr>
+            <tr className={(this.state.bmival < 18.5) ? "selection" : "nothing"} id='lasttr'>
+              <td className="firstcol">40 vagy több</td>
+              <td>III. FOKÚ ELHÍZÁS</td>
+            </tr>
+          </table>
+        </div>
+        <h2 className="orangehp">AZ IDEÁLIS TESTTÖMEGE:  KG</h2>
+        <span>Kívánatos szélső értékek</span>
+      </div>
+    );
+  }
+}
 export default App;
